b715df3a1ccfc11190cb206f25ce56f66ff7853c
Simple6969 <98166070+Simple6969@users.noreply.github.com>
2022-01-29 21:20:38 +0700

    Update sewa.js

--- shop/sewa.js
+++ shop/sewa.js
@@ -3,17 +3,17 @@ return `「 *PRICE LIST* 」
 							
 ⬣ PRICE SEWABOT
 
-⬡ 1 MINGGU = Rp 5.000
-⬡ 2 MINGGU = Rp 8.000
-⬡ 3 MINGGU = Rp 10.000
-⬡ 1 BULAN = Rp 15.000
-⬡ PERMANEN = Rp 100.000
-⬡ PERMANEN + USER PREM = Rp 150.000
+⬡ 1 MINGGU = -
+⬡ 2 MINGGU = -
+⬡ 3 MINGGU = -
+⬡ 1 BULAN = -
+⬡ PERMANEN = -
+⬡ PERMANEN + USER PREM = -
 
 
 ⬣ PRICE JADIBOT
 
-⬡ EROR.
+⬡ -
 
 
 ⬣ MINAT? PM
@@ -21,7 +21,7 @@ return `「 *PRICE LIST* 」
 
 
 NOTE : 
-Bot On 24 Jam Karna Di Run Menggunakan Heroku `
+Bot On 24 Jam Karna Di Run Menggunakan Heroku`
 	}
 
 exports.pc_sewa = pc_sewa
